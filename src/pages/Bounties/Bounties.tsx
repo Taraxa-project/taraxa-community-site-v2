4f3d2afb94a78463748d1d27c7c686ffea76157a
DMachado93 <66080295+DMachado93@users.noreply.github.com>
2021-08-24 11:04:04 +0100

    bounties page

--- src/pages/Bounties/Bounties.tsx
+++ src/pages/Bounties/Bounties.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './bounties.scss';
 import { menu } from '../../global/globalVars';
-import { Sidebar, Text, Footer, RewardCard } from 'taraxa-ui';
+import { Sidebar, Text, Footer, RewardCard, Switch } from 'taraxa-ui';
 import TaraxaIcon from '../../assets/icons/taraxaIcon';
 import PinnedIcon from '../../assets/icons/pinned';
 import Header from '../../components/Header/Header';
@@ -10,6 +10,10 @@ import ExpirationIcon from '../../assets/icons/expiration';
 import BountiesIcon from './../../assets/icons/bounties';
 
 function Bounties() {
+  const [inactive, setInactive] = useState(false);
+  const onChangeInactive = () => {
+    setInactive(!inactive);
+  }
 
   return (
     <>
@@ -19,20 +23,18 @@ function Bounties() {
         <div className="bounties-content">
           <Text label="Taraxa ecosystem bounties" variant="h4" color="primary" className="bounties-title"/>
           <Text label="Earn rewards and help grow the Taraxa's ecosystem" variant="body2" color="textSecondary" className="bounties-subtitle"/>
-          <div className="cardContainer">
-            <div className="icon-title-container">
-              <PinnedIcon /> <Text label="Pinned" variant="body1" color="primary" className="icon-title"/>
-            </div>
+
+          <div className="icon-title-container">
+            <PinnedIcon /> <Text label="Pinned" variant="body1" color="primary" className="icon-title"/>
           </div>
           <div className="cardContainer">
             <RewardCard title="Incentivized testnet" description="Earn rewards for participating in running testnet nodes" onClick={() => console.log('reward')} onClickText="Learn more" reward="100,000 TARA/month" submissions={23} expiration="Never expires" SubmissionIcon={SubmissionIcon} ExpirationIcon={ExpirationIcon} />
           </div>
 
-          <div className="cardContainer">
-            <div className="icon-title-container">
-              <BountiesIcon /> <Text label="Active Bounties" variant="body1" color="primary" className="icon-title"/>
-            </div>
+          <div className="icon-title-container">
+            <BountiesIcon /> <Text label="Active Bounties" variant="body1" color="primary" className="icon-title"/>
           </div>
+          <Switch id="bountiesSwitch" name="Show inactive" value={inactive} label="Show inactive" onChange={() => onChangeInactive()} />
           <div className="cardContainer">
             <RewardCard title="Bug bounty" description="Earn rewards for participating in running testnet nodes" onClick={() => console.log('reward')} onClickText="Learn more" reward="100,000 TARA/month" submissions={23} expiration="Never expires" SubmissionIcon={SubmissionIcon} ExpirationIcon={ExpirationIcon} />
           </div>

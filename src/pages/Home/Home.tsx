9b75cf220d7d6af33bae80319b1102ef7960fc19
Leonard Mocanu <leonard.mocanu@ymail.com>
2021-09-22 23:55:41 +0300

    fixes homepage links

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { menu } from '../../global/globalVars';
-import { IconCard, Text, ToggleButton} from '@taraxa_project/taraxa-ui';
+import { useEffect, useRef, useState } from 'react';
+import { IconCard, Text, ToggleButton } from '@taraxa_project/taraxa-ui';
 import StakingIcon from '../../assets/icons/staking';
 import BountiesIcon from '../../assets/icons/bounties';
 import RedeemIcon from '../../assets/icons/redeem';
@@ -12,7 +11,7 @@ import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import './home.scss';
 import { useMediaQuery } from 'react-responsive';
-import {store, useGlobalState} from 'state-pool';
+import { useGlobalState } from 'state-pool';
 import Sidebar from '../../components/Sidebar/Sidebar';
 
 
@@ -28,16 +27,16 @@ const Home = () => {
 
   function useOutsideAlerter(ref: any) {
     useEffect(() => {
-        function handleClickOutside(event: any) {
-            if (ref.current && !ref.current.contains(event.target)) {
-                updateSidebarOpened(false);
-            }
+      function handleClickOutside(event: any) {
+        if (ref.current && !ref.current.contains(event.target)) {
+          updateSidebarOpened(false);
         }
-  
-        document.addEventListener("mousedown", handleClickOutside);
-        return () => {
-            document.removeEventListener("mousedown", handleClickOutside);
-        };
+      }
+
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
     }, [ref]);
   }
   const wrapperRef = useRef(null);
@@ -48,37 +47,37 @@ const Home = () => {
     <>
       <Header />
       <div className={isMobile ? "home-mobile" : "home"}>
-      <Sidebar  />
+        <Sidebar />
         <div className="home-content">
-          <Text label="Get started" variant="h4" color="primary" className="home-title"/>
-          <Text label="Welcome to Taraxa's community site!" variant="body2" color="textSecondary" className="home-subtitle"/>
-          {isMobile && 
-            <ToggleButton exclusive onChange={onChangeToggle} currentValue={toggleValue} data={[{value: 'earn', label: 'Earn'}, {value: 'testnet', label: 'Testnet'}]} className="toggleButton" />
+          <Text label="Get started" variant="h4" color="primary" className="home-title" />
+          <Text label="Welcome to Taraxa's community site!" variant="body2" color="textSecondary" className="home-subtitle" />
+          {isMobile &&
+            <ToggleButton exclusive onChange={onChangeToggle} currentValue={toggleValue} data={[{ value: 'earn', label: 'Earn' }, { value: 'testnet', label: 'Testnet' }]} className="toggleButton" />
           }
-          <div className={isMobile ? "home-green-stripe-mobile" : "home-green-stripe"} style={{ display: isMobile && toggleValue !== "earn" ? 'none' : 'inherit'}}>
-            <Text label="EARN" variant="body1" color="primary" className="home-title"/>
-            <Text label="Earn rewards while helping us grow." variant="body2" color="primary" className="home-subtitle"/>
+          <div className={isMobile ? "home-green-stripe-mobile" : "home-green-stripe"} style={{ display: isMobile && toggleValue !== "earn" ? 'none' : 'inherit' }}>
+            <Text label="EARN" variant="body1" color="primary" className="home-title" />
+            <Text label="Earn rewards while helping us grow." variant="body2" color="primary" className="home-subtitle" />
           </div>
-          <div className="cardContainer" style={{ display: isMobile && toggleValue !== "earn" ? 'none' : isMobile ? 'inherit' : 'flex'}}>
+          <div className="cardContainer" style={{ display: isMobile && toggleValue !== "earn" ? 'none' : isMobile ? 'inherit' : 'flex' }}>
             <IconCard title="Staking" description="Earn rewards while helping to secure Taraxa’s network."
-            onClickText="Get Started" onClickButton={() => history.push('/staking')} Icon={StakingIcon}/>
+              onClickText="Get Started" onClickButton={() => history.push('/staking')} Icon={StakingIcon} />
             <IconCard title="Bounties" description="Earn rewards while learning about Taraxa and grow it’s ecosystem."
-            onClickText="Get Started" onClickButton={() => console.log("here")} Icon={BountiesIcon}/>
+              onClickText="Get Started" onClickButton={() => history.push('/bounties')} Icon={BountiesIcon} />
             <IconCard title="Redeem" description="Redeem TARA points for $TARA tokens and cool Taraxa swag."
-            onClickText="Get Started" onClickButton={() => console.log("here")} Icon={RedeemIcon}/>
+              onClickText="Get Started" onClickButton={() => history.push('/redeem')} Icon={RedeemIcon} />
           </div>
 
-          <div className={isMobile ? "home-green-stripe-mobile" : "home-green-stripe"} style={{ display: isMobile && toggleValue !== "testnet" ? 'none' : 'inherit'}}>
-            <Text label="TESTNET" variant="body1" color="primary" className="home-title"/>
-            <Text label="Join Taraxa’s public testnet." variant="body2" color="primary" className="home-subtitle"/>
+          <div className={isMobile ? "home-green-stripe-mobile" : "home-green-stripe"} style={{ display: isMobile && toggleValue !== "testnet" ? 'none' : 'inherit' }}>
+            <Text label="TESTNET" variant="body1" color="primary" className="home-title" />
+            <Text label="Join Taraxa’s public testnet." variant="body2" color="primary" className="home-subtitle" />
           </div>
-          <div className="cardContainer" style={{ display: isMobile && toggleValue !== "testnet" ? 'none' : isMobile ? 'inherit' : 'flex'}}>
+          <div className="cardContainer" style={{ display: isMobile && toggleValue !== "testnet" ? 'none' : isMobile ? 'inherit' : 'flex' }}>
             <IconCard title="Run a node" description="Earn rewards while helping to secure Taraxa’s network."
-            onClickText="Get Started" onClickButton={() => console.log("here")} Icon={NodeIcon}/>
+              onClickText="Get Started" onClickButton={() => history.push('/node')} Icon={NodeIcon} />
             <IconCard title="Taraxa explorer" description="Explore the ledger and find the transaction’s data."
-            onClickText="Get Started" onClickButton={() => console.log("here")} Icon={ExplorerIcon}/>
+              onClickText="Get Started" onClickButton={() => window.open('https://explorer.testnet.taraxa.io/', '_blank')} Icon={ExplorerIcon} />
             <IconCard title="Deploy DApps" description="Earn rewards while learning about Taraxa and grow it’s ecosystem."
-            onClickText="Get Started" onClickButton={() => console.log("here")} Icon={DeployIcon}/>
+              onClickText="Get Started" onClickButton={() => window.open('https://sandbox.testnet.taraxa.io/', '_blank')} Icon={DeployIcon} />
           </div>
         </div>
       </div>

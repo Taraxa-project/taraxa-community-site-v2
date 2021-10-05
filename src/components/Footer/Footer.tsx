c31671f0d862c5669bf3dffe04464a383fba63c5
Leonard Mocanu <leonard.mocanu@ymail.com>
2021-10-05 18:25:20 +0300

    footer fixes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,19 +1,18 @@
 import { Footer as TFooter } from "@taraxa_project/taraxa-ui";
-import TaraxaIcon from '../../assets/icons/taraxaIcon';
 import SendIcon from "../../assets/icons/send";
 import TwitterIcon from "../../assets/icons/twitter";
 import DiscordIcon from "../../assets/icons/discord";
 
 const Footer = () => {
   return (
-    <TFooter title="TARAXA" Icon={TaraxaIcon} description="Taraxa is a public ledger platform purpose-built for audit logging of informal transactions."
+    <TFooter description="Taraxa is a public ledger platform purpose-built for audit logging of informal transactions."
       links={[
         { label: 'Privacy Policy', link: 'https://taraxa.io/privacy' }
       ]}
       items={[
         { label: 'Send', Icon: <a href="https://twitter.com/taraxa_project" target="_blank" rel="noreferrer"><SendIcon /></a> },
+        { label: 'Discord', Icon: <a href="https://discord.com/invite/WaXnwUb" target="_blank" rel="noreferrer"><DiscordIcon /></a> },
         { label: 'Twitter', Icon: <a href="https://t.me/taraxa_project" target="_blank" rel="noreferrer"><TwitterIcon /></a> },
-        { label: 'Discord', Icon: <a href="https://discord.com/invite/WaXnwUb" target="_blank" rel="noreferrer"><DiscordIcon /></a> }
       ]} />
   )
 }

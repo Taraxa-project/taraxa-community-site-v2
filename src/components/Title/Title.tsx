db206145a0975bfb9077402cac0f8d8a3a04b09b
Leonard Mocanu <leonard.mocanu@ymail.com>
2021-10-05 18:25:32 +0300

    title fixes

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -14,7 +14,7 @@ const Title = ({ title, subtitle, tooltip }: TitleProps) => {
   return (
     <div className="page-title">
       <div className="page-title-container">
-        <Text label={title} variant="h4" color="primary" className="page-title-main" />
+        <Text label={title} variant="h1" color="primary" className="page-title-main" />
         {tooltip && <Tooltip className="page-title-tooltip" title="Test" Icon={InfoIcon} />}
       </div>
       {subtitle && <Text label={subtitle} variant="body2" color="textSecondary" className="page-title-subtitle" />}

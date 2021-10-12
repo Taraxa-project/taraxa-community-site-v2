152736e07c61d234a8bd3cca03808c516b17236d
Pablo Lefort <lefortpablo@gmail.com>
2021-10-11 21:55:34 -0300

    Fix margin bottom

--- src/pages/RunNode/RunNode.tsx
+++ src/pages/RunNode/RunNode.tsx
@@ -222,7 +222,7 @@ const RunNode = () => {
           </div>}
 
         <div className="box">
-          <Text label="References" variant="h6" color="primary" className="box-title" />
+          <Text label="References" variant="h6" color="primary" className="box-title references-title" />
           <Button label="How do I install a node?" className="referenceButton" variant="contained" onClick={() => window.open('https://docs.taraxa.io/node-setup/testnet_node_setup', '_blank', 'noreferrer noopener')} />
           <Button label="How do I upgrade my node?" className="referenceButton" variant="contained" onClick={() => window.open('https://docs.taraxa.io/node-setup/upgrade-a-node/software-upgrade', '_blank', 'noreferrer noopener')} />
           <Button label="How do I reset my node?" className="referenceButton" variant="contained" onClick={() => window.open('https://docs.taraxa.io/node-setup/upgrade-a-node/data-reset', '_blank', 'noreferrer noopener')} />
